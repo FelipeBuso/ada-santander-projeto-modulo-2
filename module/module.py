287384ecd953a48b8438db941f47879359ae3c4b
Hugo Pedra <86383900+hugopedra@users.noreply.github.com>
2023-08-15 23:41:46 +0000

    Refactor-indentacao-arquivo-json

--- module/module.py
+++ module/module.py
@@ -13,7 +13,7 @@ def ler_arquivo(nome_arquivo="./banco_dados/db.json") -> None:
 def salvar_arquivo(novo_arquivo: dict, nome_arquivo="./banco_dados/db.json") -> None:
     try:
         with open(nome_arquivo, "w") as arquivo:
-            arquivo.write(json.dumps(novo_arquivo, ensure_ascii=False))
+            arquivo.write(json.dumps(novo_arquivo, ensure_ascii=False, indent=4))
     except Exception as error:
         print(str(error))
     
