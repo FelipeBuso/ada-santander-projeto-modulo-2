d1b3fdd44733f0a1541ff6eb50bc5e6304696c2a
Hugo Pedra <86383900+hugopedra@users.noreply.github.com>
2023-08-15 23:01:54 +0000

    Refactor-adiciona-tratamento-erro-module.py

--- module/module.py
+++ module/module.py
@@ -1,15 +1,20 @@
 import json
 
 
-def ler_arquivo(nome_arquivo="./banco_dados/db.json"):
+def ler_arquivo(nome_arquivo="./banco_dados/db.json") -> None:
     try:
         with open(nome_arquivo, "r") as arquivo:
             return json.loads(arquivo.read())
     except Exception as error:
-        print(error)
+        print(str(error))
         return None
 
 
-def salvar_arquivo(novo_arquivo: dict, nome_arquivo="./banco_dados/db.json"):
-    with open(nome_arquivo, "w") as arquivo:
-        arquivo.write(json.dumps(novo_arquivo, ensure_ascii=False))
+def salvar_arquivo(novo_arquivo: dict, nome_arquivo="./banco_dados/db.json") -> None:
+    try:
+        with open(nome_arquivo, "w") as arquivo:
+            arquivo.write(json.dumps(novo_arquivo, ensure_ascii=False))
+    except Exception as error:
+        print(str(error))
+    
+    return None
