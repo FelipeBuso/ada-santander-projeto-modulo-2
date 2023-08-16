97140b81a6bb30abbe38d158d3f4a158cfa7f3dd
Felipe Buso <felipe.buso@hotmail.com>
2023-08-16 15:42:40 -0300

    refactor-medicamentos-cadastro

--- medicamentos/medicamentos.py
+++ medicamentos/medicamentos.py
@@ -1,41 +1,91 @@
+from module.module import ler_arquivo, salvar_arquivo
+from laboratorios.laboratorios import Laboratorios
+from typing import Dict, List
+
+
+class ExcecaoMedicamentos(Exception):
+    def __init__(self, mensagem: str):
+        self._erro = mensagem
+        super().__init__(self._erro)
+
+
 class Medicamentos:
-    def __init__(
+    def __init__(self) -> None:
+        self._id = None
+        self._nome = None
+        self._principio_ativo = None
+        self._laboratorio = None
+        self._descricao = None
+        self._preco = None
+        self._quantidade_estoque = None
+
+    @property
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def nome(self) -> str:
+        return self._nome
+
+    @property
+    def principio_ativo(self) -> str:
+        return self.__principio_ativo
+
+    @property
+    def laboratorio(self) -> str:
+        return self._laboratorio
+
+    @property
+    def descricao(self) -> str:
+        return self._descricao
+
+    @property
+    def preco(self) -> float:
+        return self._preco
+
+    @property
+    def quantidade_estoque(self) -> float:
+        return self._quantidade_estoque
+
+    def cadastrar_medicamento(
         self,
         nome: str,
         principio_ativo: str,
         laboratorio: Laboratorios,
         descricao: str,
         preco: float,
+        quantidade_estoque: float = 0,
     ):
-        self._nome = nome
-        self._principio_ativo = principio_ativo
-        self._laboratorio = laboratorio
-        self._descricao = descricao
-        self._preco = preco
+        dados_bd = ler_arquivo()
+        id_medicamento = str(len(dados_bd["bd_medicamentos"]) + 1)
+        if dados_bd["bd_medicamentos"]:
+            for _, medicamento in dados_bd["bd_medicamentos"].items():
+                if medicamento["nome"] == nome:
+                    raise ExcecaoMedicamentos("Medicamento já cadastrato")
 
-    @property
-    def nome(self):
-        return self._nome
-
-    @property
-    def principio_ativo(self):
-        return self.__principio_ativo
-
-    @property
-    def laboratorio(self):
-        return self._laboratorio
-
-    @property
-    def descricao(self):
-        return self._descricao
-
-    @property
-    def preco(self):
-        return self._preco
+        novo_medicamento = {
+            "nome": nome,
+            "principio_ativo": principio_ativo,
+            "laboratorio": laboratorio.cnpj,
+            "descricao": descricao,
+            "preco": preco,
+            "quantidade_estoque": quantidade_estoque,
+        }
+        dados_bd["bd_medicamentos"][id_medicamento] = novo_medicamento
+        salvar_arquivo(dados_bd)
+        return "Medicamento cadastrado com sucesso"
 
 
 class MedicamentosQuimioterapicos(Medicamentos):
-    def __init__(
+    def __init__(self) -> None:
+        self._necessita_receita = None
+        super().__init__()
+
+    @property
+    def necessita_receita(self) -> bool:
+        return self._necessita_receita
+
+    def cadastrar_medicamento(
         self,
         nome: str,
         principio_ativo: str,
@@ -43,22 +93,29 @@ class MedicamentosQuimioterapicos(Medicamentos):
         descricao: str,
         preco: float,
         necessita_receita: bool,
+        quantidade_estoque: float = 0,
     ):
-        self._necessita_receita = necessita_receita
-        super().__init__(nome, principio_ativo, laboratorio, descricao, preco)
+        dados_bd = ler_arquivo()
+        id_medicamento = str(len(dados_bd["bd_medicamentos"]) + 1)
+        if dados_bd["bd_medicamentos"]:
+            for _, medicamento in dados_bd["bd_medicamentos"].items():
+                if medicamento["nome"] == nome:
+                    raise ExcecaoMedicamentos("Medicamento já cadastrato")
 
-    @property
-    def necessita_receita(self):
-        return self._necessita_receita
+        novo_medicamento = {
+            "nome": nome,
+            "principio_ativo": principio_ativo,
+            "laboratorio": laboratorio.cnpj,
+            "descricao": descricao,
+            "preco": preco,
+            "quantidade_estoque": quantidade_estoque,
+            "necessita_receita": necessita_receita,
+        }
+        dados_bd["bd_medicamentos"][id_medicamento] = novo_medicamento
+        salvar_arquivo(dados_bd)
+        return "Medicamento cadastrado com sucesso"
 
 
 class MedicamentosFitoterapicos(Medicamentos):
-    def __init__(
-        self,
-        nome: str,
-        principio_ativo: str,
-        laboratorio: Laboratorios,
-        descricao: str,
-        preco: float,
-    ):
-        super().__init__(nome, principio_ativo, laboratorio, descricao, preco)
+    def __init__(self) -> None:
+        super().__init__()
